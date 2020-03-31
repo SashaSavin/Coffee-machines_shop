47e0682bf64bce0ee9b18a6e27fa0875a81bbd36
sashasavin <praise1234@mail.ru>
2020-04-01 01:54:14 +0300

    local vue was added(correct2)

--- message_logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-import time
-
-date = time.asctime()
-
-def logg_this(mess_to_log):
-    logging.basicConfig(filename="logs/logs - " + str(date) + ".log", level=logging.INFO, filemode='w')
-    logging.info(mess_to_log)
